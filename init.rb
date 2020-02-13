97c46d98ae491a8ea038ab880987e8417efb77c0
ShadowHD33RUS <ShadowHD33rus@gmail.com>
2020-02-13 18:22:15 +0300

    Update init.rb, create ru local file

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require_relative './lib/clipboard_hook_listener'
 
 Redmine::Plugin.register :redmine_issue_clipboard do
   name 'Redmine Issue Clipboard plugin'
-  author 'Krzysztof Kucharski'
+  author 'Krzysztof Kucharski, ShadowHD'
   description 'Copy commit message to clipboard with just one click.'
   version '0.1.0'
-  url 'https://github.com/blid/redmine_issue_clipboard'
-  author_url 'http://kucharski.net.pl'
+  url 'https://github.com/ShadowHD33RUS/redmine_issue_clipboard'
+  author_url 'http://shadowhd.ru'
 end
